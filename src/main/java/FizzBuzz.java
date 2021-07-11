fe5a6425da31e7efd83101bfaba6c57b51a7cd56
Juliano <juliano.vieira@ubots.com.br>
2021-07-10 21:32:15 -0300

    8- Validando teste FizzBuzz, (verde)

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,8 +1,18 @@
 public class FizzBuzz {
-    public String execute(int value){
-        if(fizz(value)) return "Fizz";
-        if(buzz(value)) return "Buzz";
-        return "Fizz";
+
+    public String execute(int value) {
+        String var = Integer.toString(value);
+        if (fizz(value))
+            var = "Fizz";
+        if (buzz(value)){
+            if (var.equals("Fizz")) {
+                var = "FizzBuzz";
+            } else {
+                var = "Buzz";
+            }
+        }
+
+        return var;
     }
 
     private boolean fizz(int value){
