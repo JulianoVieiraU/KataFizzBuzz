8071a9298e0ce502cfff442930cadfa1d1ad0c08
Juliano <juliano.vieira@ubots.com.br>
2021-07-10 21:05:21 -0300

    5- refatorando e criando metodo fizz, (verde)

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,11 @@
 public class FizzBuzz {
     public String execute(int value){
-        if(value %3 == 0) return "Fizz";
+        if(fizz(value)) return "Fizz";
         if(value %5 == 0) return "Buzz";
         return "Fizz";
+    }
+
+    private boolean fizz(int value){
+        return value %3 == 0;
     }
 }
