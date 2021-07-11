e740cf72372b9ec67dc3592799117e8e7b3b5277
Juliano <juliano.vieira@ubots.com.br>
2021-07-10 21:34:37 -0300

    9- ultimo teste, se nao for divisivel nem por 3 nem por 5, (verde)

--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -10,5 +10,6 @@ public class TestFizzBuzz {
         assertEquals("Fizz", fizzBuzz.execute(3));
         assertEquals("Buzz", fizzBuzz.execute(5));
         assertEquals("FizzBuzz", fizzBuzz.execute(15));
+        assertEquals("1", fizzBuzz.execute(1));
     }
 }
